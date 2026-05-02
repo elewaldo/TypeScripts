911847f0226740494345228e15ecf1e60878e9a3
elewaldo <101295181+elewaldo@users.noreply.github.com>
2026-05-02 01:27:10 +0000

    Corrección error extends en book.ts

--- Library/Outfile/book.js
+++ Library/Outfile/book.js
@@ -1,11 +1,9 @@
 export { Book };
-import { Author } from "./author.js";
-class Book extends Author {
+class Book {
     title;
     isbn;
     Author;
     constructor(title, author, isbn) {
-        super(author.name);
         this.title = title;
         this.isbn = isbn;
         this.Author = author;
--- Library/book.ts
+++ Library/book.ts
@@ -1,15 +1,12 @@
 export { Book };
-import { Author } from "./author.js";
 import { IAuthor, IBook } from "./model.js";
 
-class Book extends Author implements IBook {
+class Book implements IBook {
     title: string;
     isbn: string;
     Author: IAuthor;
 
     constructor(title: string, author: IAuthor, isbn: string) {
-        super(author.name);
-
         this.title = title;
         this.isbn = isbn;
         this.Author = author;
